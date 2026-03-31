1ddbd13cf892942f696eaf26ca26acb10e74cbb6
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-03-31 13:44:08 +0530

    EmptyViewSizing

---
+++ CollectionView2WinUI/TestSampleEmptyView/TestEmptyView/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TestEmptyView;
+
+public partial class MainPage : ContentPage
+{
+	public ObservableCollection<string> Items { get; set; } = new();
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = this;
+		// Uncomment below to add sample items and see the CollectionView populated
+		// Items.Add("Item 1");
+		// Items.Add("Item 2");
+	}
+}
