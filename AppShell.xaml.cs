13373cc1eda4dfa1d8eef62e4a2cb3a5df91b8b7
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2026-03-31 12:43:39 +0530

    Revert "Selection Changes"
    This reverts commit b58f0a0d9754b6fba7bdb5a7d7180ab4501a4ae7.

--- AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestSample;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TestSample;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
